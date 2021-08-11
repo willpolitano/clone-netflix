5fa8aa2bb23f2473f159c2626d83233b71590419
willpolitano <willianpolitano@gmail.com>
2021-08-10 22:16:09 -0300

    Listagem de filmes e categorias

--- src/App.js
+++
@@ -1,1 +0,0 @@
-https://api.themoviedb.org/3/movie/550?api_key=38c007f28d5b66f36b9c3cf8d8452a4b&language=pt-BR
--- src/Tmdb.js
+++ src/Tmdb.js
@@ -2,7 +2,7 @@ const API_KEY = '38c007f28d5b66f36b9c3cf8d8452a4b'
 const API_BASE = 'https://api.themoviedb.org/3'
 
 const basicFetch = async (endpoint) => {
-    const req = await fetch(`${API_KEY}${endpoint}`)
+    const req = await fetch(`${API_BASE}${endpoint}`)
     const json = await req.json()
     return json
 }
